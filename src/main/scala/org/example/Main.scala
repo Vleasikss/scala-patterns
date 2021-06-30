6f3b72bc2a5bdc715963172615dcffb4123a43b1
Vlas <42597604+Vleasikss@users.noreply.github.com>
2021-06-30 05:42:42 -0900

    Fabric-method-INIT

--- src/main/scala/org/example/Main.scala
+++ src/main/scala/org/example/Main.scala
@@ -1,7 +1,36 @@
 package org.example
 
+
+trait Animal {
+  def doSound():String
+}
+
+private class Dog extends Animal {
+  override def doSound(): String = "BARK"
+}
+
+private class Cat extends Animal {
+  override def doSound(): String = "MEOW"
+}
+
+object Animal {
+  val DOG = "dog"
+  val CAT = "cat"
+
+  /* Allows to get current realization of Animal using constants */
+  def apply(kind: String): Animal = kind match {
+    case DOG => new Dog
+    case CAT => new Cat
+  }
+}
+
 object Main {
   def main(args: Array[String]): Unit = {
+    val cat: Animal = Animal(Animal.CAT)
+    val dog: Animal = Animal(Animal.DOG)
+
+    println(cat.doSound())
+    println(dog.doSound())
 
   }
 }
