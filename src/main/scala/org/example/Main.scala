105f95387db2e32e06ad41fbe5d9a43aba91a12d
Vlas <42597604+Vleasikss@users.noreply.github.com>
2021-06-30 06:07:13 -0900

    added scala-strategy-dev-INIT

--- src/main/scala/org/example/Main.scala
+++ src/main/scala/org/example/Main.scala
@@ -1,19 +1,25 @@
 package org.example
 
-object Main {
-  type Strategy = (Int, Int) => Int
 
-  class Context(computer: Strategy) {
-    def use(a: Int, b: Int): Unit = {
+
+object Main extends String {
+  type Strategy[T <: AnyVal] = (T, T) => T
+
+
+  class Context[T <: AnyVal](implicit computer: Strategy[T]) {
+    def use(a: T, b: T): Unit = {
       computer(a, b)
     }
   }
 
   def main(args: Array[String]): Unit = {
     // (val1, val2) => val1 + val2
-    val add: Strategy = _ + _
-    val multiply: Strategy = _ * _
+//    val add: Strategy = _ + _
+    implicit val add: Strategy[Int] = _ + _
+    implicit val addStrings: Strategy[Float] = _ + _
 
-    new Context(multiply).use(2, 3)
+    new Context[Int]().use(2, 3)
+    new Con
+
   }
 }
