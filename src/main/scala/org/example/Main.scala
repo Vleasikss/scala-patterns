47ec07cae1d74602c208a1a2a63748eee77c7d78
Vlas <42597604+Vleasikss@users.noreply.github.com>
2021-06-30 05:59:57 -0900

    added scala-strategy-INIT

--- src/main/scala/org/example/Main.scala
+++ src/main/scala/org/example/Main.scala
@@ -1,7 +1,19 @@
 package org.example
 
 object Main {
-  def main(args: Array[String]): Unit = {
+  type Strategy = (Int, Int) => Int
 
+  class Context(computer: Strategy) {
+    def use(a: Int, b: Int): Unit = {
+      computer(a, b)
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    // (val1, val2) => val1 + val2
+    implicit val add: Strategy = _ + _
+    implicit val multiply: Strategy = _ * _
+
+    new Context(multiply).use(2, 3)
   }
 }
